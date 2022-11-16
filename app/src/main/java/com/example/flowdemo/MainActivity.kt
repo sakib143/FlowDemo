e0aaae3eebd760a42d932953a147ad0508f7001b
mohd_sakib <sakib.syed@aiegroup.org>
2022-11-16 14:53:54 +0400

    mutable flow added

--- app/src/main/java/com/example/flowdemo/MainActivity.kt
+++ app/src/main/java/com/example/flowdemo/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.flowdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.*
-import java.util.concurrent.Flow
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,29 +17,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        GlobalScope.launch {
-            producer().map {
-                Log.d("flow_demo","Map thread ${Thread.currentThread().name}")
-                it * 2 }
-                .filter {
-                    Log.d("flow_demo","Filter thread ${Thread.currentThread().name}")
-                    it < 8
-                }
-                .flowOn(Dispatchers.IO)
-                .collect {
-                    Log.d("flow_demo","Collect thread ${Thread.currentThread().name}")
-                }
+        GlobalScope.launch(Dispatchers.Main) {
+            val result = producer()
+            result.collect {
+                Log.d("==>","First consumer ??? ${it}")
+            }
+        }
+
+        GlobalScope.launch(Dispatchers.Main) {
+            val result = producer()
+            delay(2500)
+            result.collect {
+                Log.d("==>","Second consumer ??? ${it}")
+            }
         }
     }
 
     //Creating top level function by name of floow like below.
-    fun producer() = flow<Int> {
-        val array = listOf(1, 2, 3, 4, 5)
-        array.forEach {
-            delay(1000)
-            emit(it)
+    private fun producer() : Flow<Int> {
+        val mutableSharedFlow = MutableSharedFlow<Int>()
+        GlobalScope.launch {
+            val list = listOf<Int>(1,2,3,4,5)
+            list.forEach {
+                mutableSharedFlow.emit(it)
+                delay(1000)
+            }
         }
-    }.catch {
-
+        return mutableSharedFlow
     }
 }
